ff4115e9d883e4cf7c4eed044cfc58f89f0d4e57
Jeffrey Briceno <jbriz506pz@gmail.com>
2020-02-09 20:54:28 -0600

    wip: sidebar animations

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,14 +4,25 @@ import { useRef, useState } from 'react'
 import { OneOrMoreChildren } from 'types'
 import { motion } from 'framer-motion'
 import { MenuButton } from '@theme-ui/components'
+import { useBreakpointIndex } from '@theme-ui/match-media'
 
 const sideVariants = {
   open: { width: 280 },
   closed: { width: 40 },
 }
+const topVariants = {
+  open: { height: 150 },
+  closed: { height: 40 },
+}
+const transition = {
+  x: { type: 'spring', stiffness: 400 },
+  opacity: { ease: 'easeIn' },
+}
 export default function Layout({ children }: OneOrMoreChildren) {
   const [isOpen, setOpen] = useState(false)
   const containerRef = useRef(null)
+  const breakPointIndex = useBreakpointIndex()
+
   return (
     <div
       sx={{
@@ -24,7 +35,8 @@ export default function Layout({ children }: OneOrMoreChildren) {
         ref={containerRef}
         initial={false}
         animate={isOpen ? 'open' : 'closed'}
-        variants={sideVariants}
+        transition={transition}
+        variants={breakPointIndex !== 0 ? sideVariants : topVariants}
         sx={{
           flexGrow: 1,
           flexBasis: 'sidebar',
